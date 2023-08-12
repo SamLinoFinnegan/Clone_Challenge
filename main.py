c6682be75f1fd9039b2bfb1676e31b808d2ea0ba
Samuel L Finnegan <73761381+SamLinoFinnegan@users.noreply.github.com>
2023-08-12 22:12:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 import shutil
 import logging
 import logging.handlers
-
+from crontab import CronTab
 
 # add the path to your project folder
 FULLPATH ="place/the/full/path/to/your/dir"
@@ -20,16 +20,16 @@ def parse_arguments():
     """
     Argparser to be able to take arguments from the command line and send it to the clone function
     """
-    parser = argparse.ArgumentParser(description="This argparser will take arguments to syncronize two folders")
-    parser.add_argument("-PO", "--path_origin", required=True, type=str, help="Use the -PO flag for path of the origin folder.")
-    parser.add_argument("-PT", "--path_target", required=True, type=str, help="Use the -PT flag for path of the target folder.")
-    parser.add_argument("-t", "--time", nargs='?', const=0, type=str,help="Use the -t flag to set up your syncronization time, this is set in minutes")
+    parser = argparse.ArgumentParser(description="This argparser will take paths as arguments to synchronize two folders")
+    parser.add_argument("-PO", "--path_origin", required=True, type=str, help="Use the -PO flag for the path of the origin folder.")
+    parser.add_argument("-PT", "--path_target", required=True, type=str, help="Use the -PT flag for the path of the target folder.")
+    parser.add_argument("-t", "--time", nargs='?', const=0, type=str,help="Use the -t flag to set up your synchronization time, this is set in minutes")
     return parser.parse_args()
 
 def cron_job(time, command_input):
     """
-    Funtion to create the cronjob, this function takes two parameters, time, and command
-    Time should be inputed as minutes
+    Function to create the cronjob, this function takes two parameters, time, and command
+    Time should be input as minutes
     """
     with CronTab(user='root') as cron:
         job = cron.new(command=command_input)
@@ -38,17 +38,16 @@ def cron_job(time, command_input):
 def clone_folder(path_origin, path_target):
     """
     This is a program that will link two folders, copying whatever is in one to the other
-    It takes in two arguments, path_origin and path_target
+    It takes in two arguments, path_origin, and path_target
     """
     logging.info(f"Cloning files from {path_origin} to {path_target}")
 
     # remove the previous content from the target folder
-    for target_file in os.listdir(path_target):
-        target_path = os.path.join(path_target, target_file)
+    for file in os.listdir(path_target):
+        target_path = os.path.join(path_target, file)
         if os.path.isdir(target_path):
             shutil.rmtree(target_path)
             logging.info(f"Removing {target_file} folder from {path_target}")
-
         else:
             os.remove(target_path)
             logging.info(f"Removing {target_file} file from {path_target}")
@@ -74,4 +73,4 @@ if __name__ == "__main__":
     if args.time:
         # add the path to your project folder
         command = f"cd /home/Clone_Challange && /usr/bin/python3.10 main.py -PO {args.origin} -PT {args.target}"
-        # cron_job(args.time, command)
+        cron_job(args.time, command)
